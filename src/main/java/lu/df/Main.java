6b6927952d1c3b8ccf6932c41f33afd46f75f5b6
Ilja Repko <ilja.repko@gmail.com>
2024-01-17 21:29:55 +0200

    Initial commit

---
+++ src/main/java/lu/df/Main.java
@@ -0,0 +1,15 @@
+package lu.df;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Hello world from Logger!");
+        LOGGER.debug("Hello from debug!");
+    }
+}
